7ef8e9640862e7a163bd0de3b51c41b6d5291612
Evgeny <yurta3v@mail.ru>
2022-08-09 10:16:20 +0300

    removing redundant and adding linters

--- app/controllers/about_me_controller.rb
+++ app/controllers/about_me_controller.rb
@@ -2,19 +2,4 @@
 
 class AboutMeController < ApplicationController
   def index; end
-  def index
-  end
-    def new
-  end
-
-  def create
-    OrderMailer.contact_me_mail(contact_me_params).deliver_now
-    redirect_to root_path
-  end
-
-  private
-
-  def contact_me_params
-    params.permit(:first_name, :email, :message_body)
-  end
 end

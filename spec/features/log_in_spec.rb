d8eeb37b8c62d1b64986cafa7e3540f8fc326712
Evgeny <yurta3v@mail.ru>
2022-08-18 17:35:44 +0300

    page with articles and other changes

--- spec/features/log_in_spec.rb
+++ spec/features/log_in_spec.rb
@@ -8,11 +8,15 @@ RSpec.describe 'Log in' do
   feature 'Sign in' do
     scenario 'Sign in with correct params' do
       visit root_path
-      click_on 'Войти'
-      fill_in 'Email', with: user.email
-      fill_in 'Password', with: '123456'
-      click_button 'Log in'
-      expect(page).to have_content 'Signed in successfully'
+      click_on 'Вход'
+
+      #fill_in 'Email', :with => user.email
+      #fill_in 'Пароль', :with => '123456'
+
+      #fill_in 'user_email', with: user.email
+      #fill_in 'user_password', with: '123456'
+      #click_button 'Войти'
+      expect(page).to have_content 'AAaaaaa'
     end
   end
 end

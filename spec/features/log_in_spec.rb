5d6b5544136ed17c2e0bb07a14c3223c80e93a51
Evgeny <yurta3v@mail.ru>
2022-08-25 16:07:03 +0300

    fixing login test

--- spec/features/log_in_spec.rb
+++ spec/features/log_in_spec.rb
@@ -9,8 +9,8 @@ RSpec.describe 'Log in', js: true do
     scenario 'Sign in with correct params' do
       visit new_user_session_path
 
-      fill_in 'Email', with: user.email
-      fill_in 'Password', with: user.password
+      fill_in 'user_email', with: user.email
+      fill_in 'user_password', with: user.password
 
       # fill_in 'user_email', with: user.email
       # fill_in 'user_password', with: '123456'

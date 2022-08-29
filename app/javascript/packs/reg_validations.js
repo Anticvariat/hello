382f4acbcd9ec01769efdd4e877189a342877e17
Evgeny <yurta3v@mail.ru>
2022-08-29 11:33:30 +0300

    fixing issues

--- app/javascript/packs/reg_validations.js
+++ app/javascript/packs/reg_validations.js
@@ -1,35 +1,32 @@
 function isValid() {
   let email = document.getElementById("email").value;
-  let pas = document.getElementById("password").value;
-  let pasconf = document.getElementById("password_confirm").value;
+  let password = document.getElementById("password").value;
+  let password_confirm = document.getElementById("password_confirm").value;
   let first_name = document.getElementById("first_name").value;
   let last_name = document.getElementById("last_name").value;
 
-  if (email=="" || pas=="" || pasconf=="" || first_name=="" || last_name=="")
+  if (email=="" || password=="" || password_confirm=="" || first_name=="" || last_name=="")
     {
       document.getElementById("error_explanation").innerHTML = "Заполните все поля!";
       return false;
     }
 
-  if (pas != pasconf) {
-    //alert( "Пароли не совпадают!" );
+  if (password != password_confirm) {
     document.getElementById("error_explanation").innerHTML = "Пароли не совпадают!";
     return false;
   }
   else 
     {  
-      if (pas.length >= 6)
+      if (password.length >= 6)
         return true;
       else
       {
         document.getElementById("error_explanation").innerHTML = "Минимальная длина пароля 6 символов!";
-        //alert( "Минимальная длина пароля 6 символов!" );
         return false; 
       }
     }
 
 }
-
 
 document.addEventListener('submit', (event) => {
     if (isValid() == false)

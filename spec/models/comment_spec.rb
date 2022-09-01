6333ca0228e8995dc62d4f0b820dbdf72926e3f9
Evgeny <yurta3v@mail.ru>
2022-09-01 10:49:47 +0300

    fixed comments display and delete comments test

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,12 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  context 'with valid atributes' do
-    let(:article) { build(:article)}
-    let(:comment) { build(:comment) }
-
-    it 'is valid' do
-      expect(comment).to be_valid
-    end
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end

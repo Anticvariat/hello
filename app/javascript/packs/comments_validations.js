be28cf6d419832236b1d09f8f00cc206dcbfb984
Evgeny <yurta3v@mail.ru>
2022-09-05 13:41:27 +0300

    little changes in comment_validations.js

--- app/javascript/packs/comments_validations.js
+++ app/javascript/packs/comments_validations.js
@@ -1,20 +1,16 @@
 function isValid() {
   let body = document.getElementById("comment_body").value;
-
-  if (body == "")
-    {
-      document.getElementById("error_explanation").innerHTML = "Заполните все поля!";
-      return false;
-    }
-
-  if (body.length < 10) {
-    document.getElementById("errors").innerHTML = "Комментарий должен содержать не менее 10 символов!";
+  let erorrs = document.getElementById("errors")
+  
+  if (body == null || body.length < 10) {
+    errors.innerHTML = "Комментарий должен содержать не менее 10 символов!";
     return false;
   }
 
 }
+let button = document.getElementById("send_comment")
 
-document.addEventListener('submit', (event) => {
+button.addEventListener('click', (event) => {
     if (isValid() == false)
       event.preventDefault();
 });

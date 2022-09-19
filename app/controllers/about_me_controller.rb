d38cf1753e406febf0c02629de4c7601531f1426
Anticvariat <yurta3v@inbox.ru>
2022-09-19 23:11:23 +0300

    changing contact me form

--- app/controllers/about_me_controller.rb
+++ app/controllers/about_me_controller.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 class AboutMeController < ApplicationController
+  before_action :authenticate_user!
   def index; end
 
   def create
+    first_name = current_user.first_name
+    email = current_user.email
     OrderMailer.contact_me_mail(contact_me_params).deliver_now
     redirect_to root_path
   end
@@ -11,6 +14,6 @@ class AboutMeController < ApplicationController
   private
 
   def contact_me_params
-    params.permit(:first_name, :email, :message_body, :authenticity_token)
+    params.permit(:message_body, :authenticity_token)
   end
 end

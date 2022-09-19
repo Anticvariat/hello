e4121ba10db232c3d39aa8e4e2e7d17cea88c135
Evgeny <yurta3v@mail.ru>
2022-09-19 16:45:21 +0300

    changing alerts and notifications

--- app/javascript/packs/comments_validations.js
+++ app/javascript/packs/comments_validations.js
@@ -1,14 +1,14 @@
 function isValid() {
   let body = document.getElementById("comment_body").value;
-  let erorrs = document.getElementById("errors")
+  let alert = document.getElementById("alert")
   
   if (body == null || body.length < 10) {
-    errors.innerHTML = "Комментарий должен содержать не менее 10 символов!";
+    alert.innerHTML = "Комментарий должен содержать не менее 10 символов!";
     return false;
   }
 
 }
-let button = document.getElementById("send_comment")
+let button = document.getElementById("send")
 
 button.addEventListener('click', (event) => {
     if (isValid() == false)

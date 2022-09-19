6bd8c854fefea4e529a1658e0f4994a943f38263
Evgeny <yurta3v@mail.ru>
2022-09-19 10:23:41 +0300

    adding comment max length validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,5 +4,5 @@ class Comment < ApplicationRecord
   belongs_to :article
   belongs_to :user
 
-  validates :body, presence: true, length: { minimum: 5 }
+  validates :body, presence: true, length: { minimum: 5, maximum: 80 }
 end

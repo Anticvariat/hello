14034f3ff09e0f9393ddd6a8b5519a7e7227bee1
Evgeny <yurta3v@mail.ru>
2022-09-21 16:29:25 +0300

    visual changes

--- app/javascript/packs/auto_height.js
+++ app/javascript/packs/auto_height.js
@@ -1,8 +1,10 @@
 function AutoHeight(element) {
+    element.style.height = "100px";
     element.style.height = (element.scrollHeight)+"px";
 }
-let element = document.getElementById("contact-me-body")
+let element = document.getElementById("auto-height")
 
+AutoHeight(element)
 element.addEventListener('input', (event) => {
     AutoHeight(element)
 });
